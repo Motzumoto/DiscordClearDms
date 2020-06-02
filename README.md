2dd4c2e7748f4381c69c87a371f2a7e2b7ac5584
Motzumoto <45925152+Motzumoto@users.noreply.github.com>
2020-06-02 07:07:41 -0400

    A script that clears your discord dms

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # DiscordClearDms
- I found this script ages ago and i cant find it anymore so im posting it here, it can clear your discord dms, and clear all the messages in a channel that you have sent. The commands are: cleardms - it does what it says, send it anywhere and it will fetch your dms and delete them, all of them. cl - fetches the messages you sent in a channel and deletes them. 
+ I found this script ages ago and i cant find it anymore so im posting it here, it can clear your discord dms, and clear all the messages in a channel that you have sent. The commands are: cleardms - it does what it says, send it anywhere and it will fetch your dms and delete them, all of them. cl - fetches the messages you sent in a channel and deletes them.
+
+
+ if you have a lot of dms it will obviously take a while but your account should be fine, it is a self bot, so it is against tos but discord probably wont know
+
+ Have Fun and be safe out there. 
---
+++ clscript.py
@@ -0,0 +1,63 @@
+import discord, asyncio
+from colorama import Fore, Back, Style, init
+from os import system
+import shutil
+import subprocess
+
+#/////////////////////////////////////////////////////
+
+client = discord.Client()
+
+token = "PUT_TOKEN_IN_THESE_QUOTES"
+
+def tea(cmd):
+    subprocess.call(cmd, shell=True)
+#/////////////////////////////////////////////////////
+
+@client.event
+async def on_ready():
+
+    width = shutil.get_terminal_size().columns
+    cpink = Style.BRIGHT + Fore.RED
+
+    def ui():
+        tyler('')
+
+    ui()
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        commands = []
+        z = 0
+        for index, a in enumerate(message.content):
+            if a == " ":
+                commands.append(message.content[z:index])
+                z = index + 1
+        commands.append(message.content[z:])
+        channel = message.channel
+
+        width = shutil.get_terminal_size().columns
+        cpink = Style.BRIGHT + Fore.MAGENTA
+
+        if commands[0] == 'cl':
+                    if len(commands) == 1:
+                        async for msg in channel.history(limit=9999):
+                            if msg.author == client.user:
+                                try:
+                                    await msg.delete()
+                                except Exception as x:
+                                    pass
+
+        if commands[0] == 'cleardms':
+            for channel in client.private_channels:
+                if isinstance(channel, discord.DMChannel):
+                    async for msg in channel.history(limit=9999):
+                        try:
+                            if msg.author == client.user:
+                                await msg.delete()
+                                print(msg)
+                        except:
+                             pass
+client.run(token, bot=False)
